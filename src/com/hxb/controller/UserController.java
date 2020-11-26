83b2c93f6169c25faa870f559d84f8a0de34a433
和学博 <2277978888@qq.com>
2020-11-26 15:06:07 +0800

    Initial commit

---
+++ src/com/hxb/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.hxb.controller;
+
+
+import com.hxb.po.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class UserController {
+
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
+   public String  toLogin(){
+        return "login";
+    }
+
+
+    @RequestMapping(value = "/login" , method = RequestMethod.POST)
+    public String login(User user, HttpSession session, Model model){
+        String username = user.getUsername();
+        String  password=  user.getPassword();
+        if(username!=null&&username.equals("xiaoxue")&&password!=null&&password.equals("123456")){
+            session.setAttribute("USER_SESSION",user);
+            return "redirect:main";
+        }else{
+            model.addAttribute("msg","账号或密码错误");
+            return "login";
+        }
+    }
+
+    @RequestMapping(value = "/main",method = RequestMethod.GET)
+    public String main(){
+        return "main";
+    }
+
+    @RequestMapping(value = "/logout")
+    public String logout(HttpSession session){
+        session.invalidate();
+        return "login";
+    }
+}
